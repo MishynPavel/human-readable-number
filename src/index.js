98f7dc61812497b7cac8e0b40c4eedac268736fe
MishynPavel <68873173+MishynPavel@users.noreply.github.com>
2021-03-15 17:31:10 -0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+  let number__zero__twelve = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
+    number__divisible__ten = [ "" ,"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    if (number < 100) {
+      return lower__hundred (number);
+    }
+    else if (number%100 == 0) {
+      return number__zero__twelve[number/100] + " hundred"
+    }
+    else {
+      return number__zero__twelve[Math.floor(number/100)] + " hundred " + lower__hundred (number%100)
+    }
+    function lower__hundred (number) {
+      if (number < 20) {
+        return number__zero__twelve[number]
+      }
+      if (number < 100) {
+        if (number%10 == 0) {
+          return number__divisible__ten[number/10]
+        }
+        else {
+          return number__divisible__ten[Math.floor(number/10)] + " " + number__zero__twelve[number%10]
+        }
+      }
+    }
 }
